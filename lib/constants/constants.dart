cd8762ece35a0184be523ffa7960a45ba8065a51
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-22 10:45:43 -0400

    refactor: change appID type to int and implement user login/logout methods in CallServices

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const String appID = "1863826698";
+  static const int appID = 1863826698;
   static const String appSign = "e18641074e61c29900d83327dcdf859fbe2a2b4232617375190a5bf72c7d7b3c";
 }
--- lib/services/call_services.dart
+++ lib/services/call_services.dart
@@ -1,3 +1,26 @@
+import 'package:zego_uikit_app/constants/constants.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
+
 class CallServices {
-  
+  /// on App's user login
+  Future<void> onUserLogin(String userID, String userName) async {
+    /// 1.2.1. initialized ZegoUIKitPrebuiltCallInvitationService
+    /// when app's user is logged in or re-logged in
+    /// We recommend calling this method as soon as the user logs in to your app.
+    await ZegoUIKitPrebuiltCallInvitationService().init(
+      appID: Constants.appID /*input your AppID*/,
+      appSign: Constants.appSign /*input your AppSign*/,
+      userID: userID,
+      userName: userName,
+      plugins: [ZegoUIKitSignalingPlugin()],
+    );
+  }
+
+  /// on App's user logout
+  void onUserLogout() {
+    /// 1.2.2. de-initialization ZegoUIKitPrebuiltCallInvitationService
+    /// when app's user is logged out
+    ZegoUIKitPrebuiltCallInvitationService().uninit();
+  }
 }
